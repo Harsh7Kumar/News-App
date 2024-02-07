7285de9e1fbb821b4dce6f2a69aded41f74f3a18
Harsh Kumar <harsh9810kumar@gmail.com>
2024-02-07 20:11:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/project/newsapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.project.newsapp;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    TabItem mhome, msciece, mhealth, mtech, mentertainment, msports;
+    PagerAdapter pagerAdapter;
+    Toolbar mtoolbar;
+    String api = "b2f50c3280e144d99e94175bee08f636";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.back));
+        }
+
+
+        mtoolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+        mhome = findViewById(R.id.home);
+        msciece = findViewById(R.id.science);
+        mhealth = findViewById(R.id.health);
+        mtech = findViewById(R.id.technology);
+        mentertainment = findViewById(R.id.entertainment);
+        msports = findViewById(R.id.sports);
+
+        ViewPager viewPager = findViewById(R.id.fragmentContainer);
+        tabLayout = findViewById(R.id.include);
+
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager(), 6);
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2 || tab.getPosition() == 3 || tab.getPosition() == 4 || tab.getPosition() == 5)
+                    pagerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
---
+++ app/src/main/java/com/project/newsapp/webView.java
@@ -0,0 +1,37 @@
+package com.project.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class webView extends AppCompatActivity {
+    Toolbar toolbar;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        toolbar = findViewById(R.id.toolbar);
+        webView = findViewById(R.id.webView);
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.back));
+        }
+    }
+}
